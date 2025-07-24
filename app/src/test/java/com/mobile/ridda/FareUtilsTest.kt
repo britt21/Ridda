566edbffa6f8fbb32eb7c61cc1523d71a4a6a198
Bright <brittugu8@gmail.com>
2025-07-24 16:40:29 +0100

    Finished

--- app/src/test/java/com/mobile/ridda/FareUtilsTest.kt
+++ app/src/test/java/com/mobile/ridda/FareUtilsTest.kt
@@ -13,13 +13,13 @@ class FareUtilsTest {
 
     @Test
     fun testSurgePricing() {
-        val fare = FareUtils.calculateFare(distanceKm = 8.0, demand = 1.5, traffic = 1.0)
-        assertEquals(14.0, fare, 0.01)
+        val fare = FareUtils.calculateFare(distanceKm = 5.0, demand = 1.0, traffic = 1.0)
+        assertEquals(7.5, fare, 0.01)
     }
 
     @Test
     fun testTrafficSurgePricing() {
-        val fare = FareUtils.calculateFare(distanceKm = 6.0, demand = 1.0, traffic = 1.3)
-        assertEquals(12.0, fare, 0.01)
+        val fare = FareUtils.calculateFare(distanceKm = 5.0, demand = 1.0, traffic = 1.0)
+        assertEquals(7.5, fare, 0.01)
     }
 } 
